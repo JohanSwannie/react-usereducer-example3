79c5a06d3c4f997ffe8331d9e33545e99e2f4f3f
jswan <jswannieman@gmail.com>
2023-07-22 09:56:13 +1200

    Commit Execution Javascript Component

--- src/Execution.js
+++ src/Execution.js
@@ -1,15 +1,51 @@
-import { ACTIONS } from './App';
+import { ACTIONS } from "./App";
 
 export default function Execution({ execution, dispatch }) {
-
   return (
     <div>
-      <div style={{display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', margin: 'auto'}}>
-          <button className='hide'></button>
-          <button className='show' onClick={() => dispatch({ type: ACTIONS.TOGGLE_EXECUTION, stack: { id: execution.id }})}>Toggle</button>
-          <p style={{width: '70vw', fontSize: '37px', fontWeight: 'bold', textAlign: 'center', color: execution.done ? 'navy' : 'crimson', fontFamily: execution.done ? 'Sofia' : 'Tangerine'}}>{execution.enteredText}</p>
-          <button className='show' onClick={() => dispatch({ type: ACTIONS.DELETE_EXECUTION, stack: { id: execution.id }})}>Delete</button>
-          <button className='hide'></button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(5, 1fr)",
+          margin: "auto",
+        }}
+      >
+        <button className="hide"></button>
+        <button
+          className="show"
+          onClick={() =>
+            dispatch({
+              type: ACTIONS.TOGGLE_EXECUTION,
+              stack: { id: execution.id },
+            })
+          }
+        >
+          Toggle
+        </button>
+        <p
+          style={{
+            width: "70vw",
+            fontSize: "37px",
+            fontWeight: "bold",
+            textAlign: "center",
+            color: execution.done ? "navy" : "crimson",
+            fontFamily: execution.done ? "Sofia" : "Tangerine",
+          }}
+        >
+          {execution.enteredText}
+        </p>
+        <button
+          className="show"
+          onClick={() =>
+            dispatch({
+              type: ACTIONS.DELETE_EXECUTION,
+              stack: { id: execution.id },
+            })
+          }
+        >
+          Delete
+        </button>
+        <button className="hide"></button>
       </div>
     </div>
   );
