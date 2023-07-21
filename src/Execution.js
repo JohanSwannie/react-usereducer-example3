5c34070d452ca1a9236e434b28d7192c890f99a2
jswan <jswannieman@gmail.com>
2023-07-22 11:14:59 +1200

    Commit Execution Javascript Component

--- src/Execution.js
+++ src/Execution.js
@@ -2,51 +2,47 @@ import { ACTIONS } from "./App";
 
 export default function Execution({ execution, dispatch }) {
   return (
-    <div>
-      <div
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        margin: "auto",
+      }}
+    >
+      <button
+        className="show"
+        onClick={() =>
+          dispatch({
+            type: ACTIONS.TOGGLE_EXECUTION,
+            stack: { id: execution.id },
+          })
+        }
+      >
+        Toggle
+      </button>
+      <li
         style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(5, 1fr)",
-          margin: "auto",
+          width: "70vw",
+          fontSize: "37px",
+          fontWeight: "bold",
+          textAlign: "center",
+          color: execution.done ? "navy" : "crimson",
+          fontFamily: execution.done ? "Sofia" : "Tangerine",
         }}
       >
-        <button className="hide"></button>
-        <button
-          className="show"
-          onClick={() =>
-            dispatch({
-              type: ACTIONS.TOGGLE_EXECUTION,
-              stack: { id: execution.id },
-            })
-          }
-        >
-          Toggle
-        </button>
-        <li
-          style={{
-            width: "70vw",
-            fontSize: "37px",
-            fontWeight: "bold",
-            textAlign: "center",
-            color: execution.done ? "navy" : "crimson",
-            fontFamily: execution.done ? "Sofia" : "Tangerine",
-          }}
-        >
-          {execution.enteredText}
-        </li>
-        <button
-          className="show"
-          onClick={() =>
-            dispatch({
-              type: ACTIONS.DELETE_EXECUTION,
-              stack: { id: execution.id },
-            })
-          }
-        >
-          Delete
-        </button>
-        <button className="hide"></button>
-      </div>
+        {execution.enteredText}
+      </li>
+      <button
+        className="show"
+        onClick={() =>
+          dispatch({
+            type: ACTIONS.DELETE_EXECUTION,
+            stack: { id: execution.id },
+          })
+        }
+      >
+        Delete
+      </button>
     </div>
   );
 }
